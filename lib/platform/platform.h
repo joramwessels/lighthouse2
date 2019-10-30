d4e32fa79c692a7f2af3708aa93a489b50eb726a
joramwessels <joram.wessels@outlook.com>
2019-10-30 13:47:16 +0100

    Line drawing added

--- lib/platform/platform.h
+++ lib/platform/platform.h
@@ -78,5 +78,7 @@ void BindVBO( const uint idx, const uint N, const GLuint id );
 void CheckShader( GLuint shader, const char* vshader, const char* fshader );
 void CheckProgram( GLuint id, const char* vshader, const char* fshader );
 void DrawQuad();
+void DrawLines(std::vector<float2> verts, std::vector<float4> colors, float width=1.0f);
+void DrawLineStrip(std::vector<float2> verts, std::vector<float4> colors, float width=1.0f);
 
 // EOF
